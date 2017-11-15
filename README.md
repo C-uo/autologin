819d6d319ebb74e0cc18127a2a94ec434c580c2a
Cuo <choyuou@gmail.com>
2017-11-15 22:22:15 +0900

    添加了监听网络用的函数

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# autologin
-Autologin for Ritsumeikan-WIFI
+# Autologin for Ritsumeikan-WIFI
 
-将字典中的"your_username"和"your_password"改成自己的RAINBOW的帐号与密码
+将字典中的\'username'和\'password'改成自己的RAINBOW的帐号与密码并运行
+
+##11月15日 
+	创建monitoring分支,检查网络状态,并在需要时再次登录
+	(待解决: 登录失败时的对策,睡眠后的快速登录 等)
+	
--- autologin.py
+++ autologin.py
@@ -1,18 +1,51 @@
-# _*_coding:utf-8_*_
 # !usr/bin/env python
+# _*_coding:utf-8_*_
 
 import requests
+import re
+import time
 
-url = r"https://webauth.ritsumei.ac.jp/login.html"
-s = requests.Session()  # cookie
+username = ""
+password = ""
 
-payload = {
-        'buttonClicked': '4',
-        'err_flag': '0',
-        'info_flag': '0',
-        'network_name': 'Guest Network',
-        'username': 'your_username',
-        'password': 'your_password'}
 
-r = s.post(url, data=payload)
-print(r.text)
+def main():
+    """
+    每隔15秒检测一次网络是否畅通,并登录
+    """
+    login(username, password)
+    while True:
+        while monitor():
+            login(username, password)
+        time.sleep(15)
+
+
+def login(name, pwd):
+    """
+    将表单丢给学校的登录页面
+    """
+    url = r"https://webauth.ritsumei.ac.jp/login.html"
+    format_data = {
+            'buttonClicked': '4',
+            'err_flag': '0',
+            'info_flag': '0',
+            'network_name': 'Guest Network',
+            'username': name, 
+            'password': pwd
+            }
+    s = requests.Session()
+    login = s.post(url, data=format_data)
+    print(login.text)
+
+
+def monitor():
+    """
+    用正则抓取对象页面的标题,以此来判断是否为学校的登录页面
+    """
+    check = requests.get(r"http://www.google.co.jp")
+    pattern = re.compile(r"<TITLE>.*</TITLE>", re.I)
+    title = re.search(pattern, check.text).group()
+    return "web" in title
+
+
+main()
